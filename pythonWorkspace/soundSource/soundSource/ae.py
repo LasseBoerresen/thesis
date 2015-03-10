20b7354a93f6d4ef1331f8140155f80e2d2fb3c1
LasseBoerresen <fiffigini@gmail.com>
2015-03-10 20:24:26 +0100

    Finished BP for AE, needs to test and sort bugs.
    Signed-off-by: LasseBoerresen <fiffigini@gmail.com>

--- pythonWorkspace/soundSource/soundSource/ae.py
+++ pythonWorkspace/soundSource/soundSource/ae.py
@@ -17,10 +17,12 @@ from skimage import data, color
 image = data.lena()
 grayImg = color.rgb2gray(image)
 
-n_episodes = 1
+n_epocs = 1
 w_samples = 8
 n_samples = 100
 n_hidden = 64
+lr_alpha = 1.0
+wd_lambda = 1.0
 
 image_samples = np.ndarray((n_samples,w_samples,w_samples) )
 
@@ -70,36 +72,70 @@ W_bh = np.ndarray((len(b),n_hidden))
 W_ho = np.ndarray((n_hidden,n_out))
 W_bo = np.ndarray((len(b),n_out))
 
+#based on suggested value from ufldl.stanford.edu
+init_epsilon = 0.01
 
 #Initialize weigth matrices to random values to break symmetry
-W_ih = np.random.uniform(low=-1.0, high=1.0, size=np.shape(W_ih))
-W_bh = np.random.uniform(low=-1.0, high=1.0, size=np.shape(W_bh))
+W_ih = np.random.normal(loc=0.0, scale=init_epsilon**2, size=np.shape(W_ih))
+W_bh = np.random.normal(loc=0.0, scale=init_epsilon**2, size=np.shape(W_bh))
 
-W_ho = np.random.uniform(low=-1.0, high=1.0, size=np.shape(W_ho))
-W_bo = np.random.uniform(low=-1.0, high=1.0, size=np.shape(W_bo))
+W_ho = np.random.normal(loc=0.0, scale=init_epsilon**2, size=np.shape(W_ho))
+W_bo = np.random.normal(loc=0.0, scale=init_epsilon**2, size=np.shape(W_bo))
 
+delta_W_ih = np.zeros(np.shape(W_ih))
+delta_W_bh = np.zeros(np.shape(W_bh))
+delta_W_ho = np.zeros(np.shape(W_ho))
+delta_W_bo = np.zeros(np.shape(W_bo))
 
-for i in range(n_episodes):
+e_list = []
+
+for i in range(n_epocs):
     e = 0.0
     for j in range(n_samples):
         x = concImgSamps[j]
+        y = x        
         
         #Feed forward
-        a_h = x.dot(W_ih) + b.dot(W_bh)
-        y_h = 1/(1+np.exp(-a_h))
+        z_h = x.dot(W_ih) + b.dot(W_bh)
+        a_h = 1/(1+np.exp(-z_h))
         
-        a_o = y_h.dot(W_ho) + b.dot(W_bo)
-        y_o = 1/(1+np.exp(-a_o))
+        z_o = a_h.dot(W_ho) + b.dot(W_bo)
+        a_o = 1/(1+np.exp(-z_o))
 
-        e += 0.5*np.power(np.linalg.norm(y_o-x, ord=2),2.0)
+        e += 0.5*np.power(np.linalg.norm(a_o-y, ord=2),2.0)
+
+        #Calculate error term for each layer, backpropagated through the weights        
+        #'*' is element wise multiplocation
+        delta_o = -(y-a_o)*a_o*(1.0-a_o)
+        delta_h = (W_ho.T.dot(delta_o))*a_h*(1.0-a_h)
+        
+        #Calculate the gradie
+        grad_W_ho = delta_o.dot(a_o.T)
+        grad_W_bo = delta_o
+        
+        delta_W_ho = delta_W_ho + grad_W_ho
+        delta_W_bo = delta_W_bo + grad_W_bo
+        
+        delta_W_ih = delta_W_ih + grad_W_ih
+        delta_W_bh = delta_W_bh + grad_W_bh
+
+    W_ho = W_ho - lr_alpha*(delta_W_ho/double(n_samples) + wd_lambda*W_ho)
+    W_bo = W_bo - lr_alpha*delta_W_bo/double(n_samples)
+
+    W_ih = W_ih - lr_alpha*(delta_W_ih/double(n_samples) + wd_lambda*W_ih)
+    W_bh = W_bh - lr_alpha*delta_W_bh/double(n_samples)
+
 
     W_sum = 0.0
-    for j in range(W_ih[0]):
-        for k in range(W_ih[1]):
+    for j in range(len(W_ih[0])):
+        for k in range(len(W_ih[1])):
             W_sum += np.power(W_ih[j][k],2.0)
     
-    cost = e/double(n_samples) 
+    cost = e/double(n_samples) + wd_lambda*0.5*W_sum 
 
+    e_list.append(e)    
+
+plt.plot(e_list)
     #hip
 
 
